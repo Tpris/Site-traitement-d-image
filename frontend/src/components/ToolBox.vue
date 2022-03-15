74801a2b24011bcd4a2cfc48ab11ff35997bf633
naby <naby@pop-os.localdomain>
2022-03-15 19:55:27 +0100

    modif

--- frontend/src/components/ToolBox.vue
+++ frontend/src/components/ToolBox.vue
@@ -3,45 +3,69 @@
 </script>
 
 <template>
-  <ul id="tool-box" class="neumorphism">
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-    <li class="item">
-      <tool-box-button class="tool-box-item-appear">1</tool-box-button>
-    </li>
-  </ul>
+  <div id="container-tool-box">
+    <ul id="tool-box" class="neumorphism">
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+      <li class="item">
+        <tool-box-button class="tool-box-item-appear">1</tool-box-button>
+      </li>
+    </ul>
 
-  
+    <div id="container-options">
+      <ul id="options" class="neumorphism">
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+      </ul>
+      <a id="arrow-left" class="neumorphism neumorphism-push">&lt;</a>
+    </div>
+  </div>
+
+
 </template>
 
 <style scoped>
 .item{
   margin-top: 20px;
+}
+
+#container-tool-box{
+  display: flex;
 }
 
 #tool-box{
@@ -52,9 +76,42 @@
   align-items: center;
   flex-direction: column;
   overflow-y: scroll ;
+  z-index: 2;
 }
 
 #tool-box::-webkit-scrollbar {
   display: none;
+}
+
+#options{
+  display: flex;
+  flex-direction: column;
+  width: 20vw;
+  height: 71vh;
+  border-radius: 20px;
+  position: relative;
+  z-index: 0;
+}
+
+#container-options{
+  display: flex;
+  align-items: center;
+}
+
+#arrow-left{
+  font-size: 1.5em;
+  margin-right: 2vw;
+  position: relative;
+  right: 3vw;
+  margin-top: auto;
+  margin-bottom: auto;
+  border-radius: 15px;
+  width: 45px;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  z-index: 1;
 }
 </style>
