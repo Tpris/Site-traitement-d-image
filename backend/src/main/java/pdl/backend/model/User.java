ad4eb6cd7f6e53486661c2792192ed0a5cc25101
RedzCode <fortunemaeva2001@gmail.com>
2022-03-26 11:37:06 +0100

    Feat/ add H2 database

---
+++ backend/src/main/java/pdl/backend/model/User.java
@@ -0,0 +1,37 @@
+package pdl.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private final String username;
+    private final String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
