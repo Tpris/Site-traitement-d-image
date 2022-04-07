e9c82cfbe70af201d5da1d4076b2a6d70b04c67b
RedzCode <fortunemaeva2001@gmail.com>
2022-04-07 10:43:36 +0200

    feat/ add security

--- backend/src/main/java/pdl/backend/User/repository/UserRepository.java
+++ backend/src/main/java/pdl/backend/User/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package pdl.backend.User.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pdl.backend.User.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-
-
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
 }
