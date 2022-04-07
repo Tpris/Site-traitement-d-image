7eff84f89e75d626f64e5388f50760421bad8956
RedzCode <fortunemaeva2001@gmail.com>
2022-04-07 09:53:51 +0200

    feat/ add register and login

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -14,6 +14,18 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import("./components/ImageGetter.vue"),
     props: ({ params }) => ({ id: Number(params.id) || 0 })
   },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("./components/Login.vue"),
+    props: true
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("./components/Register.vue"),
+    props: true
+  },
 ];
 
 const router = createRouter({
