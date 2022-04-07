fd80a8dbd8322166e92c995db19fa38d3db43a3e
RedzCode <fortunemaeva2001@gmail.com>
2022-04-07 12:56:50 +0200

    feat/ add proper display

--- frontend/src/View/Gallery.vue
+++ frontend/src/View/Gallery.vue
@@ -5,12 +5,15 @@ import {api} from "../http-api";
 import {ImageType} from "@/types/ImageType";
 
 const state = reactive({
-  images: Array<ImageType>()
+  images: Array<ImageType>(),
+  nbImages: 0,
+  limit: 0,
 })
 
 const getImages = async () => {
   return api.getImageList().then((data) => {
     let dataArray = data as unknown as [{}]
+    state.nbImages = (dataArray[0] as unknown as { nbImages:number }).nbImages as number
     dataArray.shift()
     return data as unknown as Array<UnwrapRef<ImageType>>;
   }).catch(e => {
@@ -21,23 +24,35 @@ const getImages = async () => {
 
 onMounted(async () => {
   state.images = await getImages()
-  console.log(state.images)
-  console.log(state.images.length)
 })
 
 </script>
 
 <template>
   <nav-bar></nav-bar>
-  <div id="main-content">
-   <!--  <div v-for="image in state.images" class="text-center" :key="image.id">
-      <Image class="img neumorphism neumorphism-push appear"
-             @click="imageClick(image)" :id="image.id"
-      />
-    </div> -->
+  <div class="gallery">
+
+    <div v-for="image in state.images" :key="image['id']" :id="image['id']" class="gallery-panel">
+      <img  :src="'/images/' + image['id']" :alt="image['name']" />
+    </div>
+
   </div>
 </template>
 
 <style scoped>
+.gallery {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  grid-gap: 1rem;
+  max-width: 80rem;
+  margin: 5rem auto;
+  padding: 0 5rem;
+}
 
+.gallery-panel img {
+  width: 100%;
+  height: 22vw;
+  object-fit: cover;
+  border-radius: 0.75rem;
+}
 </style>
